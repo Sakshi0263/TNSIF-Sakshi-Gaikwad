cbed92529e4d325f4331c817d632ffeb3ab15510
Sakshi0263 <119445369+Sakshi0263@users.noreply.github.com>
2024-07-05 13:11:50 +0530

    Add files via upload

---
+++ Char.java
@@ -0,0 +1,14 @@
+package com.company.TNSIFoundation;
+
+public class Char {
+    public static void main(String[] args) {
+        char c='S';
+        System.out.println(c);
+        char v1=40;
+        System.out.println(v1);
+        char v2='\u00A7';
+        System.out.println(v2);
+        int a='B';
+        System.out.println(a);
+    }
+}
